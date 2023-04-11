397f4fd2844b41cd510e6847417abb9f84c0044e
adaravaks <tseveleva2006@gmail.com>
2023-04-11 18:01:34 +0700

    users are now listed in database once they /start the bot

--- bot.py
+++ bot.py
@@ -1,45 +1,28 @@
 from decouple import config
 from aiogram import Bot, Dispatcher, executor, types
 import psycopg2
-
-
-try:
-    connection = psycopg2.connect(
-        host=config('HOST'),
-        user=config('USER'),
-        password=config('PASSWORD'),
-        database=config('DB_NAME')
-    )
-    connection.autocommit = True
-
-    with connection.cursor() as cursor:
-        cursor.execute(
-            'SELECT version();'
-        )
-        print(f'current ver: {cursor.fetchone()}')
-
-    with connection.cursor() as cursor:
-        cursor.execute(
-            """SELECT username, money FROM users WHERE id = 1;"""
-        )
-        print([i for i in cursor.fetchone()])
-
-except Exception as ex:
-    print(f'Error has occurred: {ex}')
-finally:
-    if connection:
-        connection.close()
-        print('Connection closed')
+from database_handler import add_user, user_exists, user_money
 
 
 bot = Bot(token=config('TOKEN'))
 dp = Dispatcher(bot=bot)
 
 
-@dp.message_handler()
-async def echo(message: types.Message):
-    await message.answer(message.text)
+@dp.message_handler(commands='start')
+async def start(message: types.Message):
+    username = message.from_user.username
+    if not user_exists(username):
+        add_user(username, money=0)
+        await message.answer('You were added to the database')
+    else:
+        await message.answer(f'You are already in the database. You are {username} and you have got {user_money(username)} money units')
 
+    await message.answer(f'Welcome to the game, {username}')
+
+
+@dp.message_handler()
+async def other(message: types.Message):
+    await message.reply('ок')
 
 if __name__ == '__main__':
     executor.start_polling(dp)
--- database_handler.py
+++ database_handler.py
@@ -2,22 +2,6 @@ import psycopg2
 from decouple import config
 
 
-def select_all_users():
-    connection = psycopg2.connect(
-        host=config('HOST'),
-        user=config('USER'),
-        password=config('PASSWORD'),
-        database=config('DB_NAME')
-    )
-    connection.autocommit = True
-
-    with connection.cursor() as cursor:
-        cursor.execute(
-            """SELECT * FROM users"""
-        )
-        return cursor.fetchall()
-
-
 def add_user(username, money):
     connection = psycopg2.connect(
         host=config('HOST'),
@@ -30,7 +14,34 @@ def add_user(username, money):
     with connection.cursor() as cursor:
         cursor.execute(
             f"""INSERT INTO users (username, money) VALUES ('{username}', {money});""")
-        return f'User {username} successfully added'
+        return f'User {username} was successfully added'
 
 
-print(add_user('Jotaro', 0))
+def user_exists(username):
+    connection = psycopg2.connect(
+        host=config('HOST'),
+        user=config('USER'),
+        password=config('PASSWORD'),
+        database=config('DB_NAME')
+    )
+    connection.autocommit = True
+
+    with connection.cursor() as cursor:
+        cursor.execute(f"""SELECT * FROM users WHERE username='{username}'""")
+        if cursor.fetchall():
+            return True
+        return False
+
+
+def user_money(username):
+    connection = psycopg2.connect(
+        host=config('HOST'),
+        user=config('USER'),
+        password=config('PASSWORD'),
+        database=config('DB_NAME')
+    )
+    connection.autocommit = True
+
+    with connection.cursor() as cursor:
+        cursor.execute(f"""SELECT money FROM users WHERE username='{username}'""")
+        return cursor.fetchone()[0]  # fetchone() returns an one-element-long tuple, which is unnecessary for me, so I just select the element
