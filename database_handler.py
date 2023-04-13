ea117302bcdf1c28d2f63e8c6eee1986a1db5ac8
adaravaks <tseveleva2006@gmail.com>
2023-04-13 20:47:40 +0700

    user_checkout now shows one's current leaderboard position

--- database_handler.py
+++ database_handler.py
@@ -44,7 +44,7 @@ def get_user_money(username):
 
     with connection.cursor() as cursor:
         cursor.execute(f"""SELECT money FROM users WHERE username='{username}'""")
-        return cursor.fetchone()[0]  # fetchone() returns an one-element-long tuple, which I have no need in, so I just select that element
+        return cursor.fetchone()[0]  # fetchone() returns an one-element-long tuple, which I have no need in, so I just select that one and only element
 
 
 def get_leaderboard():
@@ -59,3 +59,26 @@ def get_leaderboard():
     with connection.cursor() as cursor:
         cursor.execute(f"""SELECT * FROM users ORDER BY money DESC LIMIT 10""")
         return cursor.fetchall()
+
+
+def user_in_leaderboard(username):
+    connection = psycopg2.connect(
+        host=config('HOST'),
+        user=config('USER'),
+        password=config('PASSWORD'),
+        database=config('DB_NAME')
+    )
+    connection.autocommit = True
+
+    with connection.cursor() as cursor:
+        cursor.execute(f"""SELECT * FROM users ORDER BY money DESC""")
+        leaderboard = cursor.fetchall()
+
+    leaderboard_dict = {}
+    for index in range(len(leaderboard)):
+        leaderboard_dict[leaderboard[index][1]] = index + 1
+
+    try:
+        return leaderboard_dict[username]
+    except KeyError:
+        return None
--- markups.py
+++ markups.py
@@ -5,7 +5,7 @@ main_menu = InlineKeyboardMarkup(row_width=2)
 back_to_menu = InlineKeyboardMarkup(row_width=1)
 
 user_checkout_btn = InlineKeyboardButton(text='Хто я?', callback_data='user_checkout')
-show_leaderboard_btn = InlineKeyboardButton(text='Лучшие игроки', callback_data='show_leaderboard')
+show_leaderboard_btn = InlineKeyboardButton(text='Leaderboard', callback_data='show_leaderboard')
 back_to_menu_btn = InlineKeyboardButton(text='Back to main menu', callback_data='back_to_menu')
 
 main_menu.insert(user_checkout_btn)
