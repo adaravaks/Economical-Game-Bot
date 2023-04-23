b316b8fa340ac336c63796d545e19d4c20e188e9
adaravaks <tseveleva2006@gmail.com>
2023-04-23 16:20:25 +0700

    added markups for new businesses

--- markups.py
+++ markups.py
@@ -9,6 +9,14 @@ to_menus = InlineKeyboardMarkup(row_width=1)
 receive_profit = InlineKeyboardMarkup(row_width=1)
 
 buy_kiosk = InlineKeyboardMarkup(row_width=1)
+buy_apiary = InlineKeyboardMarkup(row_width=1)
+buy_carwash = InlineKeyboardMarkup(row_width=1)
+buy_cafe = InlineKeyboardMarkup(row_width=1)
+buy_cottages = InlineKeyboardMarkup(row_width=1)
+buy_tvshow = InlineKeyboardMarkup(row_width=1)
+buy_bank = InlineKeyboardMarkup(row_width=1)
+buy_pmc = InlineKeyboardMarkup(row_width=1)
+buy_spacecolonies = InlineKeyboardMarkup(row_width=1)
 
 user_checkout_btn = InlineKeyboardButton(text='👤 Мой профиль', callback_data='user_checkout')
 show_leaderboard_btn = InlineKeyboardButton(text='🏆 Список богачей', callback_data='show_leaderboard')
@@ -24,8 +32,33 @@ roulette_btn = InlineKeyboardButton(text='🔴⚫ Рулетка', callback_data
 
 check_profit_btn = InlineKeyboardButton(text='💰 Прибыль от предприятий', callback_data='check_profit')
 receive_profit_btn = InlineKeyboardButton(text='🤑 Забрать', callback_data='receive_profit')
+
 check_kiosk_btn = InlineKeyboardButton(text='🗞 Киоск с газетами', callback_data='check_kiosk')
 buy_kiosk_btn = InlineKeyboardButton(text='👍 Купить', callback_data='buy_kiosk')
+
+check_apiary_btn = InlineKeyboardButton(text='🍯 Пчелиная пасека', callback_data='check_apiary')
+buy_apiary_btn = InlineKeyboardButton(text='👍 Купить', callback_data='buy_apiary')
+
+check_carwash_btn = InlineKeyboardButton(text='🚗🧼 Автомойка', callback_data='check_carwash')
+buy_carwash_btn = InlineKeyboardButton(text='👍 Купить', callback_data='buy_carwash')
+
+check_cafe_btn = InlineKeyboardButton(text='🍛 Кафе', callback_data='check_cafe')
+buy_cafe_btn = InlineKeyboardButton(text='👍 Купить', callback_data='buy_cafe')
+
+check_cottages_btn = InlineKeyboardButton(text='🏘 Коттеджный посёлок', callback_data='check_cottages')
+buy_cottages_btn = InlineKeyboardButton(text='👍 Купить', callback_data='buy_cottages')
+
+check_tvshow_btn = InlineKeyboardButton(text='📽 ТВ-шоу со звёздами', callback_data='check_tvshow')
+buy_tvshow_btn = InlineKeyboardButton(text='👍 Купить', callback_data='buy_tvshow')
+
+check_bank_btn = InlineKeyboardButton(text='🏦 Банк', callback_data='check_bank')
+buy_bank_btn = InlineKeyboardButton(text='👍 Купить', callback_data='buy_bank')
+
+check_pmc_btn = InlineKeyboardButton(text='💣 Частная военная компания', callback_data='check_pmc')
+buy_pmc_btn = InlineKeyboardButton(text='👍 Купить', callback_data='buy_pmc')
+
+check_spacecolonies_btn = InlineKeyboardButton(text='🚀 Колонизация космоса', callback_data='check_spacecolonies')
+buy_spacecolonies_btn = InlineKeyboardButton(text='👍 Купить', callback_data='buy_spacecolonies')
 
 main_menu.insert(user_checkout_btn)
 main_menu.insert(show_leaderboard_btn)
@@ -53,3 +86,27 @@ receive_profit.insert(to_main_menu_btn)
 
 buy_kiosk.insert(buy_kiosk_btn)
 buy_kiosk.insert(to_main_menu_btn)
+
+buy_apiary.insert(buy_apiary_btn)
+buy_apiary.insert(to_main_menu_btn)
+
+buy_carwash.insert(buy_carwash_btn)
+buy_carwash.insert(to_main_menu_btn)
+
+buy_cafe.insert(buy_cafe_btn)
+buy_cafe.insert(to_main_menu_btn)
+
+buy_cottages.insert(buy_cottages_btn)
+buy_cottages.insert(to_main_menu_btn)
+
+buy_tvshow.insert(buy_tvshow_btn)
+buy_tvshow.insert(to_main_menu_btn)
+
+buy_bank.insert(buy_bank_btn)
+buy_bank.insert(to_main_menu_btn)
+
+buy_pmc.insert(buy_pmc_btn)
+buy_pmc.insert(to_main_menu_btn)
+
+buy_spacecolonies.insert(buy_spacecolonies_btn)
+buy_spacecolonies.insert(to_main_menu_btn)
